54e170cee4d287b45f7ae625bace64c31478b762
Jesse Chan <jc@lineageos.org>
2017-06-02 08:28:02 +0800

    g4-common: enable insecure adb WARNING: Do NOT enable this on production builds. ONLY for debugging purposes....
    Change-Id: I96a9997f7c13b12c6dcc9be90cd5114687e37f68
    (cherry picked from commit 4b9da8351b4df132ea50ff65c2e3d9b08878d7cb)

--- product/usb.mk
+++ product/usb.mk
@@ -1,6 +1,16 @@
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Default.prop overrides to get adb working at boot
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+	ro.secure=0 \
+	ro.adb.secure=0 \
+	ro.hardware=universal8890 \
+	persist.service.adb.enable=1 \
+	persist.service.debuggable=1 \
+	persist.sys.usb.config=adb \
+	ro.securestorage.support=false
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
