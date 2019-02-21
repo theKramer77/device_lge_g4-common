a7d3613f4503df3869cd21adb16d156f248c728b
Michael Bestas <mkbestas@lineageos.org>
2019-02-21 22:23:09 +0200

    g4-common: Switch to hwbinder camera provider
    We need to keep android.hardware.camera.provider@2.4-external-service as otherwise camera is broken.
    
    Change-Id: Id1aa346135c51438cbdf11ab88e016c8be6aaf19

--- product/camera.mk
+++ product/camera.mk
@@ -2,6 +2,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-external-service \
     camera.default \
     libqomx_core \
