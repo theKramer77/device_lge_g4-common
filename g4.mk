be247282cd024b08c5f98d9f13fecc0945f9b850
Steve Kondik <shade@chemlab.org>
2019-05-01 15:15:44 +0200

    g4-common: Increase heap start size to 16m to minimize GC with larger bitmaps
    Change-Id: I89af5e94c9aafdc6007c84d3fce60e4db349e967
    (cherry picked from commit 005389ced0c44a9424e7aebfd533ac5f73164cfc)

--- g4.mk
+++ g4.mk
@@ -34,7 +34,7 @@ include $(LOCAL_PATH)/product/*.mk
 
 # Dalvik heap
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=12m \
+    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=288m \
     dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.85 \
