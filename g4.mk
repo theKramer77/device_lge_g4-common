22f3f3de561c12bb5e147d7bfe0ae8fb83c396b2
kessaras <paul@cypheros.co>
2019-07-06 10:30:12 +0300

    g4-common: hals: Remove the call for android.hardware.radio@1.0-service hal
    Because the service hal doesn't exist so it can't be built
    
    Change-Id: I28744a6b42c5799e74eced4efb239ef7abd4b6ed

--- g4.mk
+++ g4.mk
@@ -325,7 +325,6 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.0-impl \
-    android.hardware.radio@1.0-service \
     android.hardware.radio.config@1.0-service \
     android.hardware.radio.deprecated@1.0-impl \
     android.hardware.radio.deprecated@1.0-service \
